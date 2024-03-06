0754398059dd50c05afa1dc11e22d88ce159cc66
sahilsh4rma <sahilsh4rma@gmail.com>
2024-03-07 01:16:55 +0530

    adding search functionality ,desingning navbar

--- ecomsite/urls.py
+++ ecomsite/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index,name = "index")
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Products
 # Create your views here.
+
+
+def index(request):
+    product_objects =Products.objects.all()
+    product_name = request.GET.get("item_name")
+    if product_name != "" and product_name is not None:
+        product_objects = product_objects.filter(title__icontains = product_name )
+    return render(request,'shop/index.html',{'product_objects' : product_objects})
