d2d55f35ff172d2706a6ee9ed0f84ee111d5191c
sahilsh4rma <sahilsh4rma@gmail.com>
2024-03-15 01:21:46 +0530

    total price functionality completed and cart bug resolved

--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,7 @@ def index(request):
     product_name = request.GET.get("item_name")
     if product_name != "" and product_name is not None:
         product_objects = product_objects.filter(title__icontains = product_name )
-    paginator = Paginator(product_objects,4)
+    paginator = Paginator(product_objects,1)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
     return render(request,'shop/index.html',{'product_objects' : product_objects})
@@ -27,7 +27,8 @@ def checkout(request):
         city =request.POST.get("city","")
         state =request.POST.get("state","")
         zipcode =request.POST.get("zipcode","")
-        order = Order(items = items, name = name ,email = email ,address = address ,city = city ,state = state ,zipcode = zipcode)
+        total =request.POST.get("total","")
+        order = Order(items = items, name = name ,email = email ,address = address ,city = city ,state = state ,zipcode = zipcode, total = total)
         order.save()
 
     return render(request,'shop/checkout.html')
